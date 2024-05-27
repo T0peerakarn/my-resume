a76b07b50a8a239fa90bb96f74178ceb912aaa7a
PeerakarnT <6422780047@g.siit.tu.ac.th>
2024-05-27 21:00:50 +0700

    Update ui

--- src/components/InternshipsList.jsx
+++ src/components/InternshipsList.jsx
@@ -7,6 +7,11 @@ const InternshipsList = ({ internships }) => {
             justifyContent='center'
             paddingTop='3vh'
             gap='1em'
+            templateColumns={{
+                base: 'repeat(1, 1fr)',
+                sm: 'repeat(2, 1fr)',
+                md: 'repeat(3, 1fr)'
+            }}
         >
             {internships.map((internship, i) => <Internship key={i} internship={internship}/>)}
         </Grid>
